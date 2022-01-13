f63f0f890d5661ff0a9cef23b2e0ef5eed484921
Kiana Liu <kianaliu1@gmail.com>
2022-01-12 18:48:48 -0700

    q11 modified script to download and print

--- versionScript.py
+++ versionScript.py
@@ -1,4 +1,4 @@
 import requests
 
-x = requests.get('http://google.com/')
-print(x)
+x = requests.get('https://raw.githubusercontent.com/KianaLiu1/CMPUT404LAB1/main/versionScript.py')
+print(x.text)
